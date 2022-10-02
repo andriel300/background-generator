3984d0dc474d922756ab6f6be5884ae30b2a6fbd
andriel300 <andrielscript@gmail.com>
2022-10-02 13:06:14 -0300

    add random button background generator

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let css = document.querySelector("h3");
-let color1 = document.querySelector("#color1");
-let color2 = document.querySelector("#color2");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
+let randomButton = document.getElementById("random");
 
 function setGradient() {
     body.style.background =
@@ -10,6 +11,23 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
+// generating random Color in hex
+function getRandomColor() {
+    let hexColor = Math.random().toString(16);
+    hexColor = "#" + hexColor.slice(2, 8);
+    return hexColor;
+}
 
+// setting background color after button click
+function randomColors() {
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+
+    setGradient();
+}
+// default background gradient displayed on load
+setGradient();
+
+color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+randomButton.addEventListener("click", randomColors);
