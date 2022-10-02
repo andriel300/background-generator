cbc3c2d9fe18b47ef7dc1a25a247e95e343c50ec
andriel300 <andrielscript@gmail.com>
2022-10-02 12:25:20 -0300

    add text content script

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let body = document.getElementById("gradient");
 function setGradient() {
     body.style.background =
         "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+    css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
